7e7f2edf1133a55ced906475f1dad6207325e9e7
Pierfilippo Quartarella <pier.quarta25@icloud.com>
2026-07-20 09:56:08 +0200

    chore: bypass csrf per api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\SecurityHeaders::class,
             \App\Http\Middleware\SetLocale::class,
         ]);
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
